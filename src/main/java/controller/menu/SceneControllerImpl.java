93db7712e7af0e4ea111582fc45d6c4079fda98e
Giacomo <giacomo.totaro2@studio.unibo.it>
2021-03-09 22:37:17 +0100

    prova

--- src/main/java/controller/menu/SceneControllerImpl.java
+++ src/main/java/controller/menu/SceneControllerImpl.java
@@ -2,6 +2,8 @@ package controller.menu;
 
 import java.io.IOException;
 
+import com.sun.glass.ui.Cursor;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.ImageCursor;
 import javafx.scene.Parent;
@@ -12,54 +14,29 @@ import javafx.stage.Stage;
 
 public class SceneControllerImpl implements SceneController {
 
-<<<<<<< HEAD
     private final Stage currentStage;
+
+    private final int cursorDimesion = 30;
 	
     public SceneControllerImpl(final Stage currentStage) {
         this.currentStage = currentStage;
     }
 
+    /**
+     * Method that allows to change the current scene having an stage.
+     */
     @Override
     public void switchScene(final String title, final String url, final double width, final double height) {
         try {
-
-            Parent parent = FXMLLoader.load(getClass().getResource(url));
-
-            Scene newScene = new Scene(parent, width, height);
-            Image cursor = new Image(this.getClass().getResourceAsStream("/Images/Cursor/PacmanCursor.png"));
-            newScene.setCursor(new ImageCursor(cursor));
+            final Parent parent = FXMLLoader.load(getClass().getResource(url));
+            final Scene newScene = new Scene(parent, width, height);
+            final Image cursor = new Image(this.getClass().getResourceAsStream("/Images/Cursor/PacmanCursor.png"));
+            newScene.setCursor(new ImageCursor(cursor, this.cursorDimesion, this.cursorDimesion));
             this.currentStage.setScene(newScene);
             this.currentStage.setTitle(title);
             this.currentStage.show();
-
-            } catch (Exception e) {
+            } catch (IOException e) {
                 e.printStackTrace();
-            }
-    }
-=======
-        private final int cursorDimesion = 30;
-        private final Stage currentStage;
-
-        public SceneControllerImpl(final Stage primaryStage) {
-            this.currentStage = primaryStage;
-        }
-
-        /**
-         * Method that allows to change the current scene having an stage.
-         */
-        @Override
-        public void switchScene(final String title, final String url, final double width, final double height) {
-            try {
-                    final Parent parent = FXMLLoader.load(getClass().getResource(url));
-                    final Scene newScene = new Scene(parent, width, height);
-                    final Image cursor = new Image(this.getClass().getResourceAsStream("/Images/Cursor/PacmanCursor.png"));
-                    newScene.setCursor(new ImageCursor(cursor, this.cursorDimesion, this.cursorDimesion));
-                    this.currentStage.setScene(newScene);
-                    this.currentStage.setTitle(title);
-                    this.currentStage.show();
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
         }
->>>>>>> 0a9aa46824b74ba9186c375b7205d144bbaa561e
 }
