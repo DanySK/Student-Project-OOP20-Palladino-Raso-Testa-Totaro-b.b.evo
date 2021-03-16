fa700374031b300c5cfcdb00ccfd8ab98f6563aa
Alex <alex.testa@studio.unibo.it>
2021-03-16 23:52:20 +0100

    Change class player and test it

--- src/main/java/model/leaderboard/Player.java
+++ src/main/java/model/leaderboard/Player.java
@@ -2,11 +2,25 @@ package model.leaderboard;
 
 public interface Player {
 
+    /**
+     *  Method that allows to add or subtract the score to the player.
+     *  @param value - represents the value that must be added or subtract to the score.
+     *
+     */
     void scoreOperation(int value);
 
+    /**
+     *  Return a String that represents the alias of the player.
+     *  @return a String that represents the alias of the player.
+     *
+     */
     String getAlias();
 
+    /**
+     *  Return an Integer that represents the value of the player score.
+     *  @return an Integer that represents the value of the player score.
+     *
+     */
     int getScore();
 
-    int getLife();
 }
--- src/main/java/model/leaderboard/PlayerImpl.java
+++ src/main/java/model/leaderboard/PlayerImpl.java
@@ -1,42 +1,130 @@
 package model.leaderboard;
 
-public class PlayerImpl implements Player {
+public class PlayerImpl implements Player, Life {
 
+    private static final int MAX_LIFE = 3;
     private final String alias;
     private int score;
-    private Life playerLife;
+    private int life;
 
-    public PlayerImpl(final String alias, final int score, final Life life) {
+    public PlayerImpl(final String alias, final int score, final int life) {
         this.alias = alias;
         this.score = score;
-        this.playerLife = life;
+        this.life = life;
     }
 
+    /**
+     *  Method that allows to add or subtract the score to the player.
+     *  @param value - represents the value that must be added or subtract to the score.
+     *
+     */
     @Override
     public void scoreOperation(final int value) {
-        if (this.playerLife.isAlive()) {
+        if (this.isAlive() && this.getScore() > 0) {
             this.score += value;
         }
     }
 
+    /**
+     *  Return a String that represents the alias of the player.
+     *  @return a String that represents the alias of the player.
+     *
+     */
     @Override
     public String getAlias() {
         return this.alias;
     }
 
+    /**
+     *  Return an Integer that represents the value of the player score.
+     *  @return an Integer that represents the value of the player score.
+     *
+     */
     @Override
     public int getScore() {
         return this.score;
     }
 
-    @Override
-    public String toString() {
-        return "[alias=" + alias + ", score=" + score + "]";
-    }
-
+    /**
+     *  Return an Integer that represents the value of the life.
+     *  @return an Integer that represents the value of the life.
+     *
+     */
     @Override
     public int getLife() {
-        return this.playerLife.getLife();
+        return this.life;
     }
+
+    /**
+     *  Method that allows to add, of a one unit, the life.
+     */
+    @Override
+    public void increaseLife() {
+        if (this.getLife() < MAX_LIFE) {
+            this.life++;
+        }
+    }
+
+    /**
+     *  Method that allows to subtract, of a one unit, the life.
+     */
+    @Override
+    public void decreaseLife() {
+        if (this.getLife() > 0) {
+            this.life--;
+        }
+    }
+
+    /**
+     *  Returns true if the value of life is major of 0.
+     *  @return true if the value of life is major of 0.
+     *
+     */
+    @Override
+    public boolean isAlive() {
+        return this.life > 0;
+    }
+
+    /**
+     *  Return a String that represents the player object.
+     *  @return a String that represents the player object.
+     *
+     */
+    @Override
+    public String toString() {
+        return "[alias = " + this.alias + ", score = " + this.score + " ,life = " + this.life + "]";
+    }
+
+    /**
+     *
+     *  Returns the hash code value for this player.
+     *  @return the hash code value for this player.
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.alias == null) ? 0 : this.alias.hashCode());
+        return result;
+    }
+
+    /**
+     *  Method that compare the specified object whit the current player.
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        final Player player = (Player) obj;
+
+        return player.getAlias().equals(this.alias);
+    }
+
 
 }
