c93b595a6d044542823a40af972cc21734b77a02
Giacomo <giacomo.totaro2@studio.unibo.it>
2021-03-21 20:42:14 +0100

    Creato la classe enum Status

--- src/main/java/model/utilities/Status.java
+++ src/main/java/model/utilities/Status.java
@@ -1,9 +1,30 @@
 package model.utilities;
 
-public class Status {
+import java.util.Arrays;
+import java.util.List;
 
-    public Status() {
-        // TODO Auto-generated constructor stub
+/**
+ * An enum which represents the {@link Status} of the various gamObj in this game.
+ */
+public enum Status {
+    /**
+     * Represent a movable gamObj that can not destroy.
+     */
+    TO_HIT,
+    /**
+     * Represent a movable entity that can destroy.
+     */
+    HIT,
+    /**
+     * Represent the last moments of hit gamObj to hit gamObj.
+     */
+    HIT_END;
+    /**
+     * Used to know all the possible status of gamObj.
+     * 
+     * @return the list of this {@link Direction}
+     */
+    public static List<Status> getStatusList() {
+        return Arrays.asList(Status.values());
     }
-
 }
