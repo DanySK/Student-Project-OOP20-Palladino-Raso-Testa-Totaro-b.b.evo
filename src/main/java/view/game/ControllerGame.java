d4b95236d4c5c695c626770402e0d3d965f74103
Giacomo <giacomo.totaro2@studio.unibo.it>
2021-03-30 09:59:01 +0200

    add controller Game

---
+++ src/main/java/view/game/ControllerGame.java
@@ -0,0 +1,14 @@
+package view.game;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ControllerGame implements Initializable {
+
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+    }
+
+}
