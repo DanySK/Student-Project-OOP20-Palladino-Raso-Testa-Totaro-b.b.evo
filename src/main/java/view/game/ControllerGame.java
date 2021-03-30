78d0124c898be69c5fc88ff7959f39d68f70f317
Giacomo <giacomo.totaro2@studio.unibo.it>
2021-03-30 11:08:28 +0200

    update game.fxml

--- src/main/java/view/game/ControllerGame.java
+++ src/main/java/view/game/ControllerGame.java
@@ -3,12 +3,48 @@ package view.game;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import controller.menu.SceneLoaderSingleton;
+import controller.sound.SoundController;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import view.utilities.PersonalFonts;
+import view.utilities.PersonalImages;
+import view.utilities.PersonalSounds;
+import view.utilities.PersonalViews;
 
 public class ControllerGame implements Initializable {
-
+     /**
+     *  Method that initialize all component of scene.
+     */
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
+        this.loadFont();
+        this.loadListener();
+        this.resizable();
+        this.loadImage();
+    }
+
+    private void loadImage() {
+    }
+
+    private void loadFont() {
+
+    }
+
+    private void loadListener() {
+    }
+
+    private void resizable() {
     }
 
 }
