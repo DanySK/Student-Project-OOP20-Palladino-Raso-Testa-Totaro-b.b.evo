a174d057fe319f59cb8d0eaffab3332df4d07517
Alex <alex.testa@studio.unibo.it>
2021-04-01 14:58:38 +0200

    Fix GameSettingsImpl and Test class

--- src/main/java/model/settings/GameSettingsImpl.java
+++ src/main/java/model/settings/GameSettingsImpl.java
@@ -4,8 +4,10 @@ import model.utilities.Difficulty;
 
 public class GameSettingsImpl implements Settings {
 
+    private static final int HASH_FIRST_OPERAND = 1231;
+    private static final int HASH_SECOND_OPERAND = 1237;
     private static final long serialVersionUID = 5553383064816848320L;
-    private final boolean isEnableoundFx;
+    private final boolean isEnableSoundFx;
     private final boolean isEnableMusic;
     private final boolean useLeftAndRight;
     private final boolean useUpAndDown;
@@ -14,15 +16,15 @@ public class GameSettingsImpl implements Settings {
     /**
      * 
      * Create a new GameSettings whit specific parameter.
-     * @param isEnableoundFx
+     * @param isEnableSoundFx
      * @param isEnableMusic
      * @param useLeftAndRight
      * @param useUpAndDown
      * @param difficulty
      */
-    public GameSettingsImpl(final boolean isEnableoundFx, final boolean isEnableMusic, final boolean useLeftAndRight,
+    public GameSettingsImpl(final boolean isEnableSoundFx, final boolean isEnableMusic, final boolean useLeftAndRight,
                             final boolean useUpAndDown, final Difficulty difficulty) {
-        this.isEnableoundFx = isEnableoundFx;
+        this.isEnableSoundFx = isEnableSoundFx;
         this.isEnableMusic = isEnableMusic;
         this.useLeftAndRight = useLeftAndRight;
         this.useUpAndDown = useUpAndDown;
@@ -36,7 +38,7 @@ public class GameSettingsImpl implements Settings {
      */
     @Override
     public boolean isEnableoundFx() {
-        return this.isEnableoundFx;
+        return this.isEnableSoundFx;
     }
 
     /**
@@ -78,5 +80,58 @@ public class GameSettingsImpl implements Settings {
     public Difficulty getDifficulty() {
         return this.difficulty;
     }
+
+    /**
+     *  Return a String that represents the settings object.
+     *  @return a String that represents the settings object.
+     *
+     */
+    @Override
+    public String toString() {
+        return "GameSettingsImpl [isEnableSoundFx=" + isEnableSoundFx + ", isEnableMusic=" + isEnableMusic
+                + ", useLeftAndRight=" + useLeftAndRight + ", useUpAndDown=" + useUpAndDown + ", difficulty="
+                + difficulty + "]";
+    }
+
+    /**
+    *
+    *  Returns the hash code value for this settings.
+    *  @return the hash code value for this settings.
+    */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((difficulty == null) ? 0 : difficulty.hashCode());
+        result = prime * result + (isEnableMusic ? HASH_FIRST_OPERAND : HASH_SECOND_OPERAND);
+        result = prime * result + (isEnableSoundFx ? HASH_FIRST_OPERAND : HASH_SECOND_OPERAND);
+        result = prime * result + (useLeftAndRight ? HASH_FIRST_OPERAND : HASH_SECOND_OPERAND);
+        result = prime * result + (useUpAndDown ? HASH_FIRST_OPERAND : HASH_SECOND_OPERAND);
+        return result;
+    }
+
+    /**
+     *  Method that compare the specified object whit the current settings.
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final GameSettingsImpl other = (GameSettingsImpl) obj;
+
+        return other.isEnableoundFx() == this.isEnableSoundFx 
+               && other.isEnableMusic() == this.isEnableMusic
+               && other.useLeftAndRight() == this.useLeftAndRight
+               && other.useUpAndDown() == this.useUpAndDown
+               && other.getDifficulty() == this.difficulty;
+    }
+
 
 }
