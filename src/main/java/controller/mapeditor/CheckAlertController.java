3d9d4454db5d41a39ce1127d5256ef5be10699b3
Alessandro Palladino <alessandro.palladin3@studio.unibo.it>
2021-04-03 13:41:05 +0200

    add check level create

--- src/main/java/controller/mapeditor/CheckAlertController.java
+++ src/main/java/controller/mapeditor/CheckAlertController.java
@@ -10,12 +10,13 @@ import javafx.scene.control.Alert.AlertType;
  */
 public class CheckAlertController {
 
-    private final Alert alert = new Alert(AlertType.WARNING);
+    private Alert alert;
 
     /**
      * Check if all fields are entered correctly. 
      */
     public void checkAllField() {
+        alert = new Alert(AlertType.WARNING);
         alert.setHeaderText("Warning");
         alert.setContentText("You must fill all fields!");
         alert.showAndWait();
@@ -25,8 +26,19 @@ public class CheckAlertController {
      * Check if the name entered is the same as a predefined level name.
      */
     public void checkLevelName() {
+        alert = new Alert(AlertType.WARNING);
         alert.setHeaderText("Warning");
         alert.setContentText("The name you have selected already belongs to a predefined level and cannot be overwritten!");
+        alert.showAndWait();
+    }
+
+    /**
+     * Check if the level was created correctly.
+     */
+    public void checkLevelCreate() {
+        alert = new Alert(AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setContentText("The level was successfully created");
         alert.showAndWait();
     }
 }
