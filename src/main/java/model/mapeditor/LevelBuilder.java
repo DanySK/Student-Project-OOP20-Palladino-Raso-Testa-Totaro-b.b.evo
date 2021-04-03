e0d116ad81c5bf19377d0c7216d84a2029c3730e
Alessandro Palladino <alessandro.palladin3@studio.unibo.it>
2021-04-03 13:40:33 +0200

    add new methods

--- src/main/java/model/mapeditor/LevelBuilder.java
+++ src/main/java/model/mapeditor/LevelBuilder.java
@@ -1,5 +1,58 @@
 package model.mapeditor;
 
+import javafx.scene.paint.Color;
+import model.utilities.Pair;
+
 public class LevelBuilder {
+
+    private String levelName;
+    private BackGround background;
+    //un eventuale music
+
+    public LevelBuilder() {
+
+    }
+
+
+    /**
+     * DA VEDERE COME AGGIUNGERE LA TEXTURE.
+     * @param x
+     * @param y
+     * @param color
+     * @param isIndestructibile
+     * @param point
+     * @param lives
+     * @return coordinates
+     */
+    public Pair<A, Boolean> brickSelected(final double x, final double y, final Color color, final boolean isIndestructibile, 
+                                            final int point, final int lives) {
+
+    }
+
+    /**
+     * @return level built
+     */
+    public Level build() {
+
+    }
+
+    /**
+     * @param levelName
+     */
+    public void setLevelName(final String levelName) {
+        this.levelName = levelName;
+    }
+
+    /**
+     * @param backGround to set
+     */
+    public void setBackGround(final String backGround) {
+        this.background = BackGround.getBackGroundByName(backGround);
+    }
+
+    public void deleteAll() {
+        // TODO Auto-generated method stub
+
+    }
 
 }
