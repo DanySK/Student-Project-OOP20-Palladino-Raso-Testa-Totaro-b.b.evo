fb946234c288a923d428845895be9e46b3d2ae46
Alessandro Palladino <alessandro.palladin3@studio.unibo.it>
2021-04-04 11:11:55 +0200

    create new package controller.game

---
+++ src/main/java/model/entities/GameBoardImpl.java
@@ -0,0 +1,69 @@
+package model.entities;
+
+import java.util.Collection;
+import java.util.Set;
+
+
+public class GameBoardImpl implements GameBoard{
+
+    private final Set<Ball> balls;
+    private final Set<Brick> bricks;
+    //private final Paddle paddle;
+    //private final Border border;
+    
+    @Override
+    public void setBalls(Collection<Ball> balls) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void addBall(Ball ball) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void setBricks(Collection<Brick> bricks) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public Set<Ball> getBalls() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<Brick> getBricks() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Border getBorder() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void removeBall(Ball ball) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void removeBrick(Brick brick) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public Border getBorder() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Border getBorder() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
