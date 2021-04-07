5034110812e2a0688d424bcd60b9f57aedcab48b
Giacomo <giacomo.totaro2@studio.unibo.it>
2021-04-07 21:49:51 +0200

    add paddleComponentPhysics

---
+++ src/main/java/model/physics/PaddleComponentPhysics.java
@@ -0,0 +1,14 @@
+package model.physics;
+
+import model.entities.GameBoardImpl;
+import model.entities.GameObject;
+
+public class PaddleComponentPhysics implements ComponentPhysics {
+
+    @Override
+    public void update(int timeElapsed, GameObject gameObject, GameBoardImpl world) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
