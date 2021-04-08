59d6296f6b9e78a72b499e372b34c44f31c83be8
Giacomo <giacomo.totaro2@studio.unibo.it>
2021-04-08 23:01:10 +0200

    modified hitEvent and EventHandler classes

--- src/main/java/controller/event/EventHandler.java
+++ src/main/java/controller/event/EventHandler.java
@@ -8,30 +8,43 @@ import controller.game.GameState;
 import model.utilities.BrickStatus;
 
 public class EventHandler {
+
     private final List<Event> eventList = new LinkedList<>();
-    
     private final GameState state;
 
     public EventHandler(final GameState state) {
         this.state = state;
     } 
-    
+
+    /**
+     * It acts in different ways based on the type of event passed to it as input.
+     */
     public void manageEvent() {
+
+        this.eventList.stream().forEach(event -> {
+            
+        });
         
+        checkGameState();
+        this.eventList.clear();
     }
-    
+
+    /**
+     * 
+     * @param e event to add to list of event
+     */
     public void addEvent(final Event e) {
         this.eventList.add(e);
     }
 
     private void checkGameState() {
-        if(state.getLives() == 0) {
+        if (state.getLives() == 0) {
             state.setPhase(GamePhase.LOST);
-        }else if(state.getBoard().getBricks().stream()
+        } else if (state.getBoard().getBricks().stream()
                                                 .filter(i -> i.getStatus().equals(BrickStatus.DESTR) )
                                                 .count() == 0) {
             //state.addBonus();
             state.setPhase(GamePhase.WIN);
         }
-    }    
+    }
 }
--- src/main/java/controller/event/HitEvent.java
+++ src/main/java/controller/event/HitEvent.java
@@ -2,15 +2,31 @@ package controller.event;
 
 import model.entities.GameObjectImpl;
 import model.utilities.Boundaries;
-import model.utilities.GameObjectType;
 
 public class HitEvent implements Event {
+
     private final GameObjectImpl gameObj;
     private final Boundaries bounds;
-    
-    public HitEvent(GameObjectImpl gameObj, Boundaries bounds) {
+
+    public HitEvent(final GameObjectImpl gameObj, final Boundaries bounds) {
         this.gameObj = gameObj;
         this.bounds = bounds;
+    }
+
+    /**
+     * 
+     * @return gameObject
+     */
+    public GameObjectImpl getGameObj() {
+        return gameObj;
+    }
+
+    /**
+     * 
+     * @return boundaries
+     */
+    public Boundaries getBounds() {
+        return bounds;
     }
 
 }
