32826aff563de68ba84ad24942725b733f859e5d
Giacomo <giacomo.totaro2@studio.unibo.it>
2021-04-09 22:23:52 +0200

    add method Sum in Position

--- src/main/java/model/utilities/Position.java
+++ src/main/java/model/utilities/Position.java
@@ -6,42 +6,66 @@ import java.util.Objects;
  * A class which represents the positions of the various entities in this game.
  */
 public class Position {
-    private final int x;
-    private final int y;
+
+    private final double x;
+    private final double y;
+
     /**
      * Keeps positions.
      * 
      * @param x value of x-axis
      * @param y value of y-axis
      */
-    public Position(final int x, final int y) {
+    public Position(final double x, final double y) {
         this.x = Objects.requireNonNull(x);
         this.y = Objects.requireNonNull(y);
     }
+
     /**
      * 
      * @return x-axis value.
      */
-    public final int getX() {
+    public final double getX() {
         return x;
     }
+
     /**
      * 
      * @return y-axis value.
      */
-    public final int getY() {
+    public final double getY() {
         return y;
     }
+
+    /**
+     * sum between two vectors.
+     * @param v the vector to be added
+     * @return vector sum of the two positions
+     */
+    public Position sum(final DirVector v) {
+        return new Position(this.x + v.getX(), this.y + v.getY());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public final int hashCode() {
+    public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + x;
-        result = prime * result + y;
+        long temp;
+        temp = Double.doubleToLongBits(x);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(y);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
         return result;
     }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public final boolean equals(final Object obj) {
+    public boolean equals(final Object obj) {
         if (this == obj) {
             return true;
         }
@@ -52,14 +76,12 @@ public class Position {
             return false;
         }
         final Position other = (Position) obj;
-        if (x != other.x) {
+        if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x)) {
+            return false;
+        } else if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y)) {
             return false;
         }
-        /*if (y != other.y) {
-            return false;
-        }
-        return true;*/
-        return y == other.y;
+        return true;
     }
     @Override
     public final String toString() {
