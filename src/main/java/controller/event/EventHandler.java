9b994c2831ee26d5849b1dcede077a8002bf61e1
Giacomo <giacomo.totaro2@studio.unibo.it>
2021-04-09 18:58:09 +0200

    update event

--- src/main/java/controller/event/EventHandler.java
+++ src/main/java/controller/event/EventHandler.java
@@ -2,18 +2,25 @@ package controller.event;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 import controller.game.GamePhase;
 import controller.game.GameState;
+import controller.sound.SoundController;
+import model.entities.Ball;
 import model.entities.Brick;
 import model.entities.Paddle;
 import model.entities.PowerUp;
+import model.utilities.Boundaries;
 import model.utilities.GameObjStatus;
+import model.utilities.ScoreAttribute;
+import view.utilities.PersonalSounds;
 
 public class EventHandler {
 
     private final List<Event> eventList = new LinkedList<>();
     private final GameState state;
+    private SoundController music;
     private int ballDamage = 1; //da importare dai settings e viene modificato dai powerup
 
     public EventHandler(final GameState state) {
@@ -27,23 +34,33 @@ public class EventHandler {
 
         this.eventList.stream().forEach(event -> {
             final HitEvent hit = (HitEvent) event;
-            if (hit.getGameObj() instanceof Brick && hit.getGameObj().getStatus() == GameObjStatus.DESTR) {
-                final Brick brick = (Brick) hit.getGameObj();
-                    brick.decreaseDurability(ballDamage);
-                    //state.addPoint(brick.getPoint);
-                    if (checkDurability(brick)) {
-                        brick.setStatus(GameObjStatus.BROKEN); //non so se ci servira'
-                        state.getBoard().removeBrick(brick);
-                    }
-
-            } else if (hit.getGameObj() instanceof PowerUp && hit.getGameObj().getStatus() == GameObjStatus.DESTR) {
-                final PowerUp pwup = (PowerUp) hit.getGameObj();
+            if (hit.getGameObj().get() instanceof Brick && hit.getGameObj().get().getStatus() == GameObjStatus.DESTR) {
+                final Brick brick = (Brick) hit.getGameObj().get();
+                brick.decreaseDurability(ballDamage);
+                state.addPoint(ScoreAttribute.BRICK_DAMAGED.getValue());            //add the score of the brick hit
+                if (checkDurability(brick)) {
+                    brick.setStatus(GameObjStatus.BROKEN); //non so se ci servira'
+                    state.addPoint(ScoreAttribute.BRICK_BREAK.getValue());          //add the score of the broken brick
+                    state.getBoard().removeBrick(brick);
+                }
+                this.music.playMusic(PersonalSounds.SOUND_BRICK.getURL().getPath());    //throw the sound for hitting the brick
+            } else if (hit.getGameObj().get() instanceof PowerUp && hit.getGameObj().get().getStatus() == GameObjStatus.DESTR) {
+                final PowerUp pwup = (PowerUp) hit.getGameObj().get();
                 pwup.decreaseDurability(ballDamage);
                 if (checkDurability(pwup)) {
-                    
+           ///////////////////////////////////////////////////
                 }
-            } else if (hit.getGameObj() instanceof Paddle) {
-                
+            } else if (hit.getGameObj().get() instanceof Paddle) {
+                this.music.playMusic(PersonalSounds.SOUND_PADDLE.getURL().getPath());    //throw sound for hitting the paddle
+            } else if (hit.getBounds().isPresent()) {
+                if (hit.getBounds().get().equals(Boundaries.LOWER)) {
+                    this.state.getBoard().removeBall((Ball) hit.getGameObj().get());
+                    if (this.state.getBoard().getBalls().isEmpty()) {
+                        this.state.decLives();
+                        this.state.setPhase(GamePhase.INIT);
+                    }
+                }
+                this.music.playMusic(PersonalSounds.SOUND_WALL.getURL().getPath());    //throw sound for hitting the wall
             }
         });
         checkGameState();
--- src/main/java/controller/event/HitEvent.java
+++ src/main/java/controller/event/HitEvent.java
@@ -1,33 +1,35 @@
 package controller.event;
 
+import java.util.Optional;
+
 import model.entities.GameObject;
 import model.entities.GameObjectImpl;
 import model.utilities.Boundaries;
 
 public class HitEvent implements Event {
 
-    private final GameObjectImpl gameObj;
-    private final Boundaries bounds;
+    private final Optional<GameObject> gameObj;
+    private final Optional<Boundaries> bounds;
 
     public HitEvent(final GameObjectImpl gameObj, final Boundaries bounds) {
-        this.gameObj = gameObj;
-        this.bounds = bounds;
+        this.gameObj = Optional.of(gameObj);
+        this.bounds = Optional.of(bounds);
     }
 
     /**
      * 
      * @return gameObject
      */
-    public GameObject getGameObj() {
-        return gameObj;
+    public Optional<GameObject> getGameObj() {
+        return this.gameObj;
     }
 
     /**
      * 
      * @return boundaries
      */
-    public Boundaries getBounds() {
-        return bounds;
+    public Optional<Boundaries> getBounds() {
+        return this.bounds;
     }
 
 }
