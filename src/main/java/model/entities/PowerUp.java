df8bb1a0346d645e2eadff270d44f45192c2cfb5
Francesco <francesco.raso@studio.unibo.it>
2021-04-09 18:08:36 +0200

    added getters and setters for powerup attributes

--- src/main/java/model/entities/PowerUp.java
+++ src/main/java/model/entities/PowerUp.java
@@ -7,9 +7,18 @@ import model.utilities.PowerUpType;
 public class PowerUp extends Brick {
     private PowerUpType pwtype;
 
+    private float activeTime;
+    private float speedModifier;
+    private int lifeModifier;
+    private int damageModifier;
+
     public PowerUp(final Position pos, final double speed, final int height, final int width, final int durability, final GameObjStatus gameObjStatus) {
         super(pos, speed, height, width, durability, gameObjStatus);
         this.pwtype = PowerUpType.randomPowerUpType();
+        this.setActiveTime(PowerUpType.valueOf(this.pwtype.toString()).getActiveTime());
+        this.setDamageModifier(PowerUpType.valueOf(this.pwtype.toString()).getDamageModifier());
+        this.setLifeModifier(PowerUpType.valueOf(this.pwtype.toString()).getLifeModifier());
+        this.setSpeedModifier(PowerUpType.valueOf(this.pwtype.toString()).getSpeedModifier());
     }
 
     /**
@@ -30,8 +39,8 @@ public class PowerUp extends Brick {
      * activates the powerup when its boundaries hit the paddle.
      * @param seconds
      */
-    public void activatePowerUp(final int seconds) {
-        
+    public void activatePowerUp() {
+
         switch (this.pwtype) {
         case DAMAGE_DOWN:
             break;
@@ -55,9 +64,73 @@ public class PowerUp extends Brick {
 
     /**
      * getter for {@link PowerUpType} attribute.
-     * @return poweruptype
+     * @return which powerup will be created
      */
     public PowerUpType getPowerUpType() {
         return this.pwtype;
+    }
+
+    /**
+     * getter for the damage modifier.
+     * @return the damage modifier 
+     */
+    public int getDamageModifier() {
+        return damageModifier;
+    }
+
+    /**
+     * setter for the damage modifier.
+     * @param damageModifier
+     */
+    public void setDamageModifier(final int damageModifier) {
+        this.damageModifier = damageModifier;
+    }
+
+    /**
+     * getter for the life modifier. 
+     * @return the life modifier
+     */
+    public int getLifeModifier() {
+        return lifeModifier;
+    }
+
+    /**
+     * setter for the life modifier.
+     * @param lifeModifier
+     */
+    public void setLifeModifier(final int lifeModifier) {
+        this.lifeModifier = lifeModifier;
+    }
+
+    /**
+     * getter for the speed modifier.
+     * @return the speed modifier
+     */
+    public float getSpeedModifier() {
+        return speedModifier;
+    }
+
+    /**
+     * setter for the speed modifier.
+     * @param speedModifier
+     */
+    public void setSpeedModifier(final float speedModifier) {
+        this.speedModifier = speedModifier;
+    }
+
+    /**
+     * getter for the active time.
+     * @return active time in seconds
+     */
+    public float getActiveTime() {
+        return activeTime;
+    }
+
+    /**
+     * setter for the active time.
+     * @param activeTime
+     */
+    public void setActiveTime(final float activeTime) {
+        this.activeTime = activeTime;
     }
 }
