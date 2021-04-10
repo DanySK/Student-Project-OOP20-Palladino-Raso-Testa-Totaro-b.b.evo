ef4220aab96206ac9667e5eaf08c7206a07ee4fb
Alessandro Palladino <alessandro.palladin3@studio.unibo.it>
2021-04-10 16:51:03 +0200

    Change parameters from int to double

--- src/main/java/model/utilities/Wall.java
+++ src/main/java/model/utilities/Wall.java
@@ -2,8 +2,8 @@ package model.utilities;
 
 public class Wall extends Position {
 
-    public Wall(int x, int y) {
-        super(x, y);
+    public Wall(final double worldWidth, final double worldHeight) {
+        super(worldWidth, worldHeight);
         // TODO Auto-generated constructor stub
-    }  
+    }
 }
