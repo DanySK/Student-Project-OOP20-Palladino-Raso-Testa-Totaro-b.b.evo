93e41006c32ccb7508715366a4dcad525d1475c3
Alessandro Palladino <alessandro.palladin3@studio.unibo.it>
2021-04-10 16:42:28 +0200

    Finish

--- src/main/java/controller/event/GameBoardEventListeners.java
+++ src/main/java/controller/event/GameBoardEventListeners.java
@@ -1,9 +1,13 @@
 package controller.event;
 
+/**
+ * 
+ * Interface for applying the pattern.
+ */
 public interface GameBoardEventListeners {
     /**
-     * 
-     * @param e
+     * Adds events to the queue that will be resolved with each iteration of gameLoop.
+     * @param e event generated
      */
     void eventListener(Event e);
 }
