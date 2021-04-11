4c6330d4bd2fc02219e649a0ca6eb27ac6c4c90e
Giacomo <giacomo.totaro2@studio.unibo.it>
2021-04-11 12:36:07 +0200

    finish paddleComponentPhysics

--- src/main/java/model/physics/PaddleComponentPhysics.java
+++ src/main/java/model/physics/PaddleComponentPhysics.java
@@ -5,6 +5,7 @@ import model.entities.GameObject;
 import model.entities.Paddle;
 import model.utilities.Boundaries;
 import model.utilities.DirVector;
+import model.utilities.GameUtilities;
 import model.utilities.Position;
 
 public class PaddleComponentPhysics implements ComponentPhysics {
@@ -14,13 +15,18 @@ public class PaddleComponentPhysics implements ComponentPhysics {
      */
     @Override
     public void update(final int timeElapsed, final GameObject gameObject, final GameBoardImpl world) {
+        final Position upperLeftCorner = GameUtilities.getUpperLeftCorner();
+        final Position bottomRightCorner = GameUtilities.getRightBottomCorner();
+
         final Paddle paddle = (Paddle) gameObject;
         final Position posPaddle = paddle.getPos();
         final DirVector dirVectPaddle = paddle.getDirVector();
         paddle.setPos(posPaddle.sum(dirVectPaddle.mul(COST_VELOCITY * timeElapsed * paddle.getSpeed())));
         if (world.checkPaddleCollisionsWithWall(paddle).isPresent()) {
             if (world.checkPaddleCollisionsWithWall(paddle).get().equals(Boundaries.SIDE_LEFT)) {
-                paddle.setPos(pos);
+                paddle.setPos(new Position(upperLeftCorner.getX(), paddle.getPos().getY()));
+            } else {
+                paddle.setPos(new Position(bottomRightCorner.getX() - paddle.getWidth(), paddle.getPos().getY()));
             }
         }
     }
