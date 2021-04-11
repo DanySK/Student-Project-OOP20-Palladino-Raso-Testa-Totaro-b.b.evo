b4272ee00f757efc0b08af9e02401fa4c045f130
Giacomo <giacomo.totaro2@studio.unibo.it>
2021-04-11 14:52:08 +0200

    finish paddle graphics

--- src/main/java/view/graphics/PaddleComponentGraphics.java
+++ src/main/java/view/graphics/PaddleComponentGraphics.java
@@ -1,15 +1,24 @@
 package view.graphics;
 
 import model.entities.GameObject;
+import model.entities.Paddle;
+import resource.routing.PersonalImages;
+import javafx.scene.image.Image;
 
 public class PaddleComponentGraphics implements ComponentGraphics {
 
+    private final Image paddleImage;
+
     public PaddleComponentGraphics() {
+        this.paddleImage = new Image(PersonalImages.PADDLE_IMG.getResourceAsStream());
     }
 
+    /**
+     *  draw the paddle by passing the specific graphic information to the graphic adapter. 
+     */
     @Override
     public void update(final GameObject obj, final AdapterGraphics graphicsAdapt) {
-        // TODO Auto-generated method stub
+        graphicsAdapt.drawPaddle((Paddle) obj, this.paddleImage);
     }
 
 }
