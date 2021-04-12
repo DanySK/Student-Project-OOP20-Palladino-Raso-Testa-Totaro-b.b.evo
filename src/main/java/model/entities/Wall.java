d91d40d183ae78a7b042c81fa61b3fc0ce2945ea
Francesco <francesco.raso@studio.unibo.it>
2021-04-12 21:57:30 +0200

    update wall

--- src/main/java/model/entities/Wall.java
+++ src/main/java/model/entities/Wall.java
@@ -1,9 +1,48 @@
-package model.utilities;
 
-public class Wall extends Position {
+package model.entities;
+
+import model.utilities.GameUtilities;
+import model.utilities.Position;
+
+public class Wall {
+
+    private final double width;
+    private final double height;
 
     public Wall(final double worldWidth, final double worldHeight) {
-        super(worldWidth, worldHeight);
-        // TODO Auto-generated constructor stub
+        this.width = worldWidth;
+        this.height = worldHeight;
+    }
+
+    /**
+     * 
+     * @return width of the wall
+     */
+    public double getWidth() {
+        return width;
+    }
+
+    /**
+     * 
+     * @return height of the wall
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * 
+     * @return the upper left corner
+     */
+    public Position getBottomRightCorner() {
+        return GameUtilities.getRightBottomCorner();
+    }
+
+    /**
+     * 
+     * @return the bottom right corner
+     */
+    public Position getUpperleftCorner() {
+        return GameUtilities.getUpperLeftCorner();
     }
 }
