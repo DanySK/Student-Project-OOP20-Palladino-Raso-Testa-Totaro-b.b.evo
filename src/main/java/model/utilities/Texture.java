ed874b7f1fcda1f86ca6ff4789601debed74bd33
Alessandro Palladino <alessandro.palladin3@studio.unibo.it>
2021-04-12 19:12:56 +0200

    Format the style and change some variable

--- src/main/java/model/utilities/Texture.java
+++ src/main/java/model/utilities/Texture.java
@@ -4,26 +4,34 @@ import resource.routing.Theme;
 
 public class Texture {
     /**
-     * 
+     * Folder for Brick texture.
      */
     public static final String BRICK_TEXTURE = "Images/background/";
+    /**
+     * Folder for PowerUp brick texture.
+     */
     public static final String BRICK_POWERUP_TEXTURE = "Images/background/";
+    /**
+     * Folder for PowerUp texture.
+     */
     public static final String POWERUP_TEXTURE = "Images/background/";
 
-    private String path;
-    private Theme currentTheme;
-    private String fileFormat = ".png";
+    /**
+     * Format of file.
+     */
+    public static final String FILE_FORMAT = ".png";
+
+    private final String path;
+    private final Theme currentTheme;
 
     /**
      * 
      * @param path
      * @param theme
-     * @param fileFormat
      */
-    public Texture(final String path, Theme theme, String fileFormat) {
+    public Texture(final String path, final Theme theme) {
         this.path = path;
         this.currentTheme = theme;
-        this.fileFormat = fileFormat;
     }
 
     /**
@@ -31,6 +39,6 @@ public class Texture {
      * @return texture path 
      */
     public String buildTexturePath() {
-        return path + currentTheme.getThemeName() + fileFormat;
+        return path + currentTheme.getThemeName() + FILE_FORMAT;
     }
 }
