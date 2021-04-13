6df5b655b23f0f14788eef387d5e8cfc4003bc3e
Francesco <francesco.raso@studio.unibo.it>
2021-04-13 20:55:51 +0200

    updated GameUtilities, added PowerUpUtilities

---
+++ src/main/java/model/utilities/PowerUpUtilities.java
@@ -0,0 +1,34 @@
+package model.utilities;
+
+public final class PowerUpUtilities {
+
+    /**
+     * default damage dealt by the ball.
+     */
+    public static final int DEFAULT_BALL_DAMAGE = 1;
+
+    /**
+     * damage modifier used by powerups to increase or decrease the damage dealt by the ball.
+     */
+    public static final int DAMAGE_MODIFIER = 1;
+
+    /**
+     * default active time for each powerup activation (seconds).
+     */
+    public static final float POWERUP_ACTIVE_TIME = 5;
+
+    /**
+     * default life moodifier used by powerups to increase or decrease player's lives.
+     */
+    public static final int DEFAULT_LIFE_MODIFIER = 1;
+
+    /**
+     * speed modifier used by powerups to speed the ball up or down.
+     */
+    public static final int SPEED_MODIFIER = 1;
+
+    private PowerUpUtilities() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
