e7ddb7c080dd0352119612a0066d5b7dad3f516a
Giacomo <giacomo.totaro2@studio.unibo.it>
2021-04-14 14:44:44 +0200

    finish TestPaddle

--- src/test/java/model/TestPaddle.java
+++ src/test/java/model/TestPaddle.java
@@ -3,17 +3,14 @@ package model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import controller.game.GameState;
-import controller.game.GameStateImpl;
 import controller.input.ControllerInput;
 import controller.input.ControllerInputImpl;
 import model.entities.GameBoard;
@@ -32,7 +29,6 @@ public class TestPaddle {
     private Paddle p1;
     private Paddle p2;
     private Paddle p3;
-    private Paddle p4;
     private final Map<Paddle, ControllerInput> inputController = new HashMap<>();
 
   /**
@@ -40,24 +36,19 @@ public class TestPaddle {
    */
   @BeforeEach
   public void createEntity() {
-      //final GameState gameState = new GameStateImpl();
       this.board = new GameBoardImpl(new Wall(600, 600), null);
       this.p1 = createPaddle(ObjectInit.PADDLE.getStartPos());
-      //this.p2 = createPlayer(new Position(600, 580));
-      //this.p3 = createPlayer(new Position(-10, 580));
-      //this.p4 = createPlayer(ObjectInit.PADDLE.getStartPos());
-      //final Set<Paddle> playerContainer = Set.of(p1, p2, p3, p4);
+      this.p2 = createPaddle(new Position(600, 580));
+      this.p3 = createPaddle(new Position(-10, 580));
       this.board.setPaddle(p1);
       this.inputController.put(p1, new ControllerInputImpl());
-      //this.inputController.put(PlayerId.TWO, new KeyboardInputController());
-
   }
 
     /**
-     * create a paddle and check that the builder sets all the fields correctly.
+     * Check that the builder sets all the fields correctly.
      */
     @Test
-    public void paddleCreation() {
+    public void checkBuilderPaddle() {
         assertEquals(ObjectInit.PADDLE.getStartPos(), p1.getPos());
         assertEquals(new DirVector(0, 0), p1.getDirVector());
         assertEquals(ObjectInit.PADDLE.getInitHeight(), p1.getHeight());
@@ -70,7 +61,6 @@ public class TestPaddle {
     @Test
     public void testPaddleCreation() {
         assertEquals(new Position(250, 540), p1.getPos());
-//      assertEquals(new P2d(190, 580), e4.getPos());
         assertEquals(78, p1.getWidth());
         assertEquals(20, p1.getHeight());
         assertThrows(IllegalStateException.class, () -> new Paddle.Builder().build());
@@ -88,64 +78,36 @@ public class TestPaddle {
     @Test
     public void testCollisionWithBoundaries() {
         assertFalse(board.checkGameObjCollisionsWithWall(p1).isPresent());
-        //assertTrue(world.checkCollisionWithBoundaries(e2).isPresent());
-        //assertTrue(world.checkCollisionWithBoundaries(e3).isPresent());
-        //assertEquals(Boundaries.SIDE_LEFT, board.checkCollisionWithBoundaries(e1).get());
-        //assertEquals(Collision.RIGHT, world.checkCollisionWithBoundaries(e2).get());
+        assertTrue(board.checkGameObjCollisionsWithWall(p2).isPresent());
+        assertTrue(board.checkGameObjCollisionsWithWall(p3).isPresent());
+        assertEquals(Boundaries.SIDE_LEFT, board.checkGameObjCollisionsWithWall(p3).get());
+        assertEquals(Boundaries.SIDE_RIGHT, board.checkGameObjCollisionsWithWall(p2).get());
   }
-    
 
     /**
-     * Test if the inputController notify correctly the movement of the player with
-     * different player id and if it moves depending on speed of player and delta time
-     * given by the gameLoop.
+     * Test if the ControllerInput notify correctly the movement of the player
+     * and if it moves depending on speed of player and time elapsed given by the gameLoop.
      */
     @Test
     public void testPlayerMovement() {
+        //move paddle right
         assertEquals(new Position(250, 540), p1.getPos());
-        //assertEquals(new Position(190, 580), e4.getPos());
         inputController.get(p1).setMoveRight(true);
-        //inputController.get(PlayerId.TWO).notifyMoveLeft(true);
         inputController.entrySet().forEach(i -> {
             board.movePaddle(i.getValue());
         });
         board.updateState(20);
-        assertEquals(new Position(259,540), p1.getPos());
-        //assertEquals(new Position(181,580), e4.getPos());
+        assertEquals(new Position(258,540), p1.getPos());
+
+        //move paddle left
+        inputController.get(p1).setMoveLeft(true);
+        inputController.entrySet().forEach(i -> {
+            board.movePaddle(i.getValue());
+        });
+        board.updateState(20);
+        assertEquals(new Position(250,540), p1.getPos());
     }
-    
-    
-    
-    
-//    /**
-//     *  put paddle into the world. Update the time to see the movement of the paddle.
-//     */
-//    @Test
-//    public void paddleMovement() {
-//        final GameBoard board = new GameBoardImpl(new Wall(100, 100), null);
-//        final Paddle.Builder paddleBuilder = new Paddle.Builder();
-//        paddleBuilder.height(ObjectInit.PADDLE.getInitHeight()).width(ObjectInit.PADDLE.getInitWidth())
-//            .texturePath(PATH);
-//
-//        // west direction
-//        double py = Math.sin(Math.toRadians(180));
-//        double px = Math.cos(Math.toRadians(180));
-//        paddleBuilder.position(new Position(50, 50)).direction(new DirVector(px, py));
-//        board.setBalls(Arrays.asList(ballBuilder.build()));
-//        assertEquals(new Position(50, 50), board.getBalls().stream().findFirst().get().getPos());
-//        board.updateState(10);
-//        assertEquals(new Position(46, 50), board.getBalls().stream().findFirst().get().getPos());
-//
-//        // east direction
-//        py = Math.sin(Math.toRadians(0));
-//        px = Math.cos(Math.toRadians(0));
-//        ballBuilder.position(new Position(50, 50)).direction(new DirVector(px, py));
-//        board.setBalls(Arrays.asList(ballBuilder.build()));
-//        assertEquals(new Position(50, 50), board.getBalls().stream().findFirst().get().getPos());
-//        board.updateState(10);
-//        assertEquals(new Position(54, 50), board.getBalls().stream().findFirst().get().getPos());
-//    }
-    
+
     private Paddle createPaddle(final Position pos) {
         return new Paddle.Builder()
                 .position(pos)
