65202025ba1d4d4b1749d15068cb1d4d6514b582
Giacomo <giacomo.totaro2@studio.unibo.it>
2021-04-15 17:12:02 +0200

    update

--- src/main/java/model/entities/Paddle.java
+++ src/main/java/model/entities/Paddle.java
@@ -15,6 +15,7 @@ import view.graphics.PaddleComponentGraphics;
 
 public final class Paddle extends GameObjectImpl {
 
+    private static final long serialVersionUID = -854724973379080675L;
     private static final double PADDLE_SPEED = 0.4;
     private final Map<GameObject, Boundaries> hit = new HashMap<>();
     private final String tPath;
