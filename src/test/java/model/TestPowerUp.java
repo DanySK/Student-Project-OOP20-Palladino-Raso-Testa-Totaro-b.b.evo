d08fc9624d470d0c183b1c50a49ee84f2261abfa
Francesco <francesco.raso@studio.unibo.it>
2021-04-16 11:15:08 +0200

    update TestPowerup

--- src/test/java/model/TestPowerUp.java
+++ src/test/java/model/TestPowerUp.java
@@ -4,21 +4,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import model.entities.Ball;
 import model.entities.GameBoard;
 import model.entities.GameBoardImpl;
 import model.entities.PowerUp;
 import model.entities.Wall;
+import model.utilities.Boundaries;
+import model.utilities.Difficulty;
+import model.utilities.DirVector;
+import model.utilities.ObjectInit;
 import model.utilities.Position;
 
 public class TestPowerUp {
     private static final Position POWERUP_POS = new Position(50, 50);
+    private static final Position NEWPOS = new Position(50, 47);
     private static final int WALL_DIM = 600;
     private static final int POWERUP_WIDTH = 10;
     private static final int POWERUP_HEIGHT = 10;
     private static final String PATH = "Images/dropPowerup/marioDropPowerUp.png";
+    private static final String PATH_POWERUP = "Images/dropPowerup/marioDropPowerUp.png";
+    private static final int TIME_ELAPSED = 10;
+
     private GameBoard board;
     private PowerUp pwup;
 
@@ -47,23 +59,34 @@ public class TestPowerUp {
     }
 
     /**
-     * Test collisions with board boundaries.
+     * Check that collisions occur with the game wall.
      */
-     /* @Test
-    public void testCollisionWithBoundaries() {
-        assertFalse(board.checkGameObjCollisionsWithWall(pwup).isPresent());
-        //assertTrue(board.checkGameObjCollisionsWithWall(pwup).isPresent());
-        assertEquals(Boundaries.LOWER, board.checkGameObjCollisionsWithWall(pwup).get());
-    }*/
+    @Test
+    public void checkPowerUpBoardCollision() {
+        this.board = new GameBoardImpl(new Wall(100, 100), null);
+        assertTrue(board.getSceneEntities().isEmpty());
+        //set powerUp pos to the bottom edge and check for a collision
+        final PowerUp powerUp = new PowerUp(new Position(50, 95), 10, 10, PATH_POWERUP);
+        assertEquals(Boundaries.LOWER, board.checkGameObjCollisionsWithWall(powerUp).get());
+        //set paddle pos to in the middle of the world and check for no collision;
+        powerUp.setPos(new Position(5,50));
+        assertEquals(Optional.empty(), board.checkGameObjCollisionsWithWall(powerUp));
+    }
 
     /**
-     * Test if the ControllerInput notify correctly the movement of the player
-     * and if it moves depending on speed of player and time elapsed given by the gameLoop.
+     * Test if the powerup drops correctly.
      */
     @Test
     public void testPowerUpMovement() {
-        //move paddle right
+        final GameBoard board = new GameBoardImpl(new Wall(WALL_DIM, WALL_DIM), null);
         assertEquals(POWERUP_POS, pwup.getPos());
+        //Powerup goes south direction
+        final DirVector dir = new DirVector(0, -1);
+        pwup.setDirVector(dir);
+        board.setPowerUps(Arrays.asList(pwup));
+        assertEquals(POWERUP_POS, board.getPowerUp().stream().findFirst().get().getPos());
+        board.updateState(TIME_ELAPSED);
+        assertEquals(NEWPOS, board.getPowerUp().stream().findFirst().get().getPos());
 
     }
 }
