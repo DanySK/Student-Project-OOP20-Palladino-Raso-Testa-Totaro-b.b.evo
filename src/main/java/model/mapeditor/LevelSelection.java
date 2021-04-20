8b3d299ff991d963d09892a204d511ee29725102
Alessandro Palladino <alessandro.palladin3@studio.unibo.it>
2021-04-20 11:19:40 +0200

    Fix texture and level selection

--- src/main/java/model/mapeditor/LevelSelection.java
+++ src/main/java/model/mapeditor/LevelSelection.java
@@ -35,12 +35,12 @@ public enum LevelSelection implements Iterator<LevelSelection> {
     /**
      * Level 5 location and the input to fit the iterator interface.
      */
-    LEVEL5("standardLevel/crash", "crash", 4, false),
+    LEVEL5("standardLevel/mario", "mario", 5, true),
 
     /**
      * Level 6 location and the input to fit the iterator interface.
      */
-    LEVEL6("standardLevel/mario", "mario", 5, true);
+    LEVEL6("standardLevel/crash", "crash", 4, false);
 
     private String path;
     private String name;
